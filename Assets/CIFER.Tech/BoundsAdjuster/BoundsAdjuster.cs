1346d0d20fc5913d763e9864388c8966620df9b4
karukaru <karukaru808@users.noreply.github.com>
2021-07-23 03:53:48 +0900

    Init

---
+++ Assets/CIFER.Tech/BoundsAdjuster/BoundsAdjuster.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace CIFER.Tech.BoundsAdjuster
+{
+    public static class BoundsAdjuster
+    {
+        public static void AdjustBounds(Animator animator)
+        {
+            var scale = GetMaxExtent(animator);
+
+            if (animator.isHuman)
+            {
+                var humanScale = animator.humanScale;
+                scale = humanScale > scale ? humanScale : scale;
+            }
+
+            foreach (var smr in animator.GetComponentsInChildren<SkinnedMeshRenderer>())
+            {
+                var bounds = smr.localBounds;
+                bounds.extents = Vector3.one * (scale * 1.25f);
+                smr.localBounds = bounds;
+            }
+        }
+
+        private static float GetMaxExtent(Component animator)
+        {
+            var smrs = animator.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+            foreach (var smr in smrs)
+            {
+                PrefabUtility.RevertPropertyOverride(new SerializedObject(smr).FindProperty("m_AABB"),
+                    InteractionMode.AutomatedAction);
+            }
+
+            var extents = smrs.Select(smr => smr.localBounds.extents);
+            var x = extents.Select(extent => extent.x).Max();
+            var y = extents.Select(extent => extent.y).Max();
+            var z = extents.Select(extent => extent.z).Max();
+
+            var value = x;
+            if (y > value)
+            {
+                value = y;
+            }
+
+            if (z > value)
+            {
+                value = z;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Assets/CIFER.Tech/BoundsAdjuster/Editor/BoundsAdjusterEditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+namespace CIFER.Tech.BoundsAdjuster.Editor
+{
+    public class BoundsAdjusterEditor : EditorWindow
+    {
+        private static Animator _animator;
+
+        [MenuItem("CIFER.Tech/BoundsAdjuster")]
+        private static void Open()
+        {
+            var window = GetWindow<BoundsAdjusterEditor>("BoundsAdjuster");
+            window.minSize = new Vector2(350f, 200f);
+        }
+
+        private void OnGUI()
+        {
+            _animator = EditorGUILayout.ObjectField("Target", _animator, typeof(Animator), true) as Animator;
+
+            if (GUILayout.Button("実行"))
+            {
+                BoundsAdjuster.AdjustBounds(_animator);
+            }
+        }
+    }
+}
